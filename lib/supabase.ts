20a089feccd944d8269b2b3102acb8346ac57832
Tomek Augustyn <blog2t@gmail.com>
2025-11-10 11:29:18 +0100

    Type fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -30,7 +30,7 @@ export type Database = {
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // Helper functions
 export async function saveReversedText(
